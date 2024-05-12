14041b0716cb506fd55848d38e2b7905745cc395
Mrz <86775732+TheMrz-01@users.noreply.github.com>
2024-05-12 14:35:33 +0300

    I'm sick so for now this is everything I can do

--- main.cpp
+++ main.cpp
@@ -6,17 +6,34 @@ int main(void)
 {
     glfwInit();
 
+
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
 
+    GLfloat vertices[] =
+    {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+
     GLFWwindow* window = glfwCreateWindow(800, 800, "Enigma Engine", NULL, NULL);
+
     if (window == NULL) {
         std::cout << "Error: Could not create window" << std::endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
+
+    gladLoadGL();
+
+    glViewport(0,0,800,800);
+    glClearColor(0.26f, 0.26f, 0.26f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
 
     while(!glfwWindowShouldClose(window))
     {
