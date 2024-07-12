f53dd5f01a0f6eacf9f47fe50ff10538e03a9910
Mrz <86775732+TheMrz-01@users.noreply.github.com>
2024-07-12 17:20:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,22 @@
 #include"VBO.h"
 #include"EBO.h"
 
+
+
 GLfloat vertices[] = {
-     0.5f,  0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f,
-    -0.5f,  0.5f, 0.0f
+    //               COORDINATES                  /     COLORS           //
+     -0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, 
+     0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, 
+     0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f,
+    -0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f,
+     0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f,
+     0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f
 };
 
 GLuint indices[] = {
-    0, 1, 3,
-    1, 2, 3
+    0, 3, 5,
+    3, 2, 4,
+    5, 4, 1
 };
 
 int main(void)
@@ -49,7 +55,8 @@ int main(void)
     VBO VBO1(vertices, sizeof(vertices));
     EBO EBO1(indices, sizeof(indices));
 
-    VAO1.LinkVBO(VBO1, 0);
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     VAO1.Unbind();
     VBO1.Unbind();
     EBO1.Unbind();
@@ -63,7 +70,7 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
         shaderProgram.Activate();
         VAO1.Bind();
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
 
         glfwPollEvents();
