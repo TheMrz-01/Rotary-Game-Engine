a42fe20b890c681345ec46a76f21df9593814d14
Mrz <86775732+TheMrz-01@users.noreply.github.com>
2024-08-18 14:41:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,19 +12,28 @@
 #include"VBO.h"
 #include"EBO.h"
 
+#define FPS 144.0f // or whatever
+#define PI 3.141692563589
 
+const unsigned int width = 800;
+const unsigned int heigth = 800;
 
 GLfloat vertices[] = {
     //        COORDINATES  /     COLORS           //
-     -0.5f, -0.5f, 0.0f,     1.0f, 0.0f,  0.0f, 0.0f, 0.0f,
-     -0.5f, 0.5f, 0.0f,     0.0f, 1.0f,  0.0f,  0.0f, 1.0f,
-     0.5f,  0.5f, 0.0f,     0.0f, 0.0f,  1.0f,  1.0f, 1.0f,
-     0.5f,  -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,  1.0f, 0.0f
+     -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+    -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+     0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+     0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+     0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
 };
 
 GLuint indices[] = {
-    0,2,1,
-    0,3,2
+    0, 1, 2,
+    0, 2, 3,
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
+    3, 0, 4
 };
 
 int main(void)
@@ -36,7 +45,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
 
 
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Rotary Game Engine", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(width, heigth, "Rotary Game Engine", NULL, NULL);
 
     if (window == NULL) {
         std::cout << "Error: Could not create window" << std::endl;
@@ -47,7 +56,7 @@ int main(void)
     glfwMakeContextCurrent(window);
     gladLoadGL();
 
-    glViewport(0,0,800,800);
+    glViewport(0,0, width,heigth);
 
     Shader shaderProgram("default.vert", "default.frag");
 
@@ -69,6 +78,16 @@ int main(void)
     //Texture
     Texture gokhan("Gokhan Hitler - Copy.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
     gokhan.texUnit(shaderProgram, "tex0", 0);
+
+    float rotation = 0.0f;
+    double prevTime = glfwGetTime();
+
+    //delta = FPS * rotationsPerSecond; // idk what to call it
+    //rotation = ((frame % delta) / delta) * 2 * PI;
+
+    glfwSwapInterval(1);
+
+    glEnable(GL_DEPTH_TEST);    
     
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
@@ -76,12 +95,40 @@ int main(void)
     while(!glfwWindowShouldClose(window))
     {
         glClearColor(0.26f, 0.26f, 0.26f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         shaderProgram.Activate();
+
+        double crntTime = glfwGetTime();
+        if (crntTime - prevTime >= 1 / 60)
+        {
+            rotation += 0.5f;
+            prevTime = crntTime;
+        }
+
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 proj = glm::mat4(1.0f);
+
+        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 0.1f, 0.0f));
+
+        view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+
+        proj = glm::perspective(glm::radians(45.0f), (float)width / heigth, 0.1f, 100.0f);
+
+        int modelLoc = glGetUniformLocation(shaderProgram.ID,"model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
         glUniform1f(uniID, 0.5f);
         gokhan.Bind();
         VAO1.Bind();
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
 
         glfwPollEvents();
